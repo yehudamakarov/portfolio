21c593f4455521e7714c7a611d57bd375c91cdc3
Yehuda Makarov <yehudamakarov@gmail.com>
2021-07-01 22:01:46 -0400

    working hero!

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,30 +1,49 @@
 import * as React from "react"
-import { makeStyles, Typography } from "@material-ui/core"
+import { Container, makeStyles, Typography } from "@material-ui/core"
 import { StaticImage } from "gatsby-plugin-image"
+import { createStyles } from "@material-ui/core/styles"
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => createStyles({
   root: {
-    height: "60vh",
-    backgroundColor: "#171b29",
     position: "relative"
   },
-  overlay: {
+  div: {
+    height: "70vh",
+    [theme.breakpoints.down("lg")]: {
+      height: "60vh"
+    },
+    width: "100vw",
+  },
+  img: {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+    objectPosition: "37% 30%",
+    [theme.breakpoints.down("lg")]: {
+      objectPosition: "10% 30%"
+    }
+  },
+  imgOverlay: {
     position: "absolute",
-    left: "50%",
-    top: "30%",
-    transform: "translate(-50%, -50%)",
-    color: "#a1a4aa"
-  }
-})
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    background: "rgba(0,0,0,0.47)"
+  },
+  overlay: {}
+}))
 
 export const Hero = () => {
   const classes = useStyles()
   return (
-    <section>
-      <div className={classes.root} />
-      <div className={classes.overlay}>
-        <Typography variant={"caption"}>welcome to my site. I'm a software craftsman. feel free to browse around, or reach out.</Typography>
+    <div className={classes.root}>
+      <StaticImage className={classes.div} imgClassName={classes.img} src="../images/quarry.jpeg" alt={""} />
+      <div className={classes.imgOverlay}>
+        <Container maxWidth={"lg"}>
+          <Typography variant={"h2"}>hey</Typography>
+        </Container>
       </div>
-    </section>
+    </div>
   )
 }
