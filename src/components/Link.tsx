f5157e980e2343d925ae6dbbe1fb0fd146c58a51
Yehuda Makarov <yehudamakarov@gmail.com>
2021-10-27 13:56:31 -0400

    ts link workaround and index ts

---
+++ src/components/Link.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import MuiLink from '@mui/material/Link';
+import {GatsbyLinkProps, Link as GatsbyLink} from 'gatsby';
+
+interface CustomGatsbyLink extends Omit<GatsbyLinkProps<Record<string, unknown>>, 'ref'> {
+}
+
+const Link = React.forwardRef<GatsbyLink<any>, CustomGatsbyLink>(function Link(props, ref) {
+    return <MuiLink component={GatsbyLink} ref={ref} {...props} />;
+});
+
+export default Link;
