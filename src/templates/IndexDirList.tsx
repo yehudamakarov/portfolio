767249e245582f917c167c66624bcdf9823d9aaf
Yehuda Makarov <yehudamakarov@gmail.com>
2022-04-05 13:26:00 -0400

    reemove animation

--- src/templates/IndexDirList.tsx
+++ src/templates/IndexDirList.tsx
@@ -1,9 +1,8 @@
 // todo give full path and current level
 import * as React from "react"
-import { Fragment, useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { WindowLocation } from "@reach/router"
 import { FoldersImmediatelyUnderQuery } from "../../gatsby-graphql"
-import { animated, config, useTrail } from "react-spring"
 import { IndexDirListEl } from "./IndexDirListEl"
 import { IndexDirListHeader } from "./IndexDirListHeader"
 import Xarrow from "react-xarrows"
@@ -15,12 +14,6 @@ export function IndexDirList(props: {
 }) {
   const theme = useTheme()
   const [width, setWidth] = useState(800)
-  const trail = useTrail(1 + props.subFolders.length, {
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-    config: config.stiff,
-    delay: 200
-  })
 
   const ref = useRef(null)
 
@@ -30,33 +23,27 @@ export function IndexDirList(props: {
 
   return (
     <>
-      {trail.map((value, index) => (
-        index !== 0 ? (
-          <Fragment key={index}>
-            <Box sx={{ pl: 5 }}>
-              <animated.div style={value}>
-                <IndexDirListEl arrowId={"listEl" + String(index)} dirName={props.subFolders[index - 1].path} />
-                <Xarrow
-                  start={"listHeader"}
-                  end={"listEl" + String(index)}
-                  lineColor={theme.palette.primary.main}
-                  strokeWidth={2}
-                  showHead={false}
-                  startAnchor={{
-                    position: "bottom",
-                    offset: { x: -((width / 2) - parseInt(theme.spacing(2).slice(0, 2))) }
-                  }}
-                  endAnchor={"left"}
-                />
-              </animated.div>
-            </Box>
-          </Fragment>
-        ) : (
-          <animated.div ref={ref} id={"listHeader"} key={index} style={value}>
-            <IndexDirListHeader dirName={props.location.pathname} />
-          </animated.div>
-        )))
-      }
+      <Box ref={ref} id={"listHeader"}>
+        <IndexDirListHeader dirName={props.location.pathname} />
+      </Box>
+      {props.subFolders.map((value, index) => (
+        <Box key={index} sx={{ pl: 5 }}>
+          <IndexDirListEl arrowId={"listEl" + String(index)} dirName={props.subFolders[index].path} />
+          <Xarrow
+            start={"listHeader"}
+            end={"listEl" + String(index)}
+            lineColor={theme.palette.primary.main}
+            strokeWidth={2}
+            showHead={false}
+            startAnchor={{
+              position: "bottom",
+              offset: { x: -((width / 2) - parseInt(theme.spacing(2).slice(0, 2))) }
+            }}
+            endAnchor={"left"}
+            curveness={1}
+          />
+        </Box>
+      ))}
     </>
   )
 }
