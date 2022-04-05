66bb1abbdca57027baaadf32e7e9d303df978e4a
Yehuda Makarov <yehudamakarov@gmail.com>
2022-04-05 16:19:56 -0400

    staggering properly

--- src/templates/IndexDirList.tsx
+++ src/templates/IndexDirList.tsx
@@ -8,6 +8,7 @@ import { IndexDirListHeader } from "./IndexDirListHeader"
 import Xarrow from "react-xarrows"
 import { Box, useTheme } from "@mui/material"
 import useMeasure from "react-use-measure"
+import { animated, config, useTransition } from "react-spring"
 
 export function IndexDirList(props: {
   location: WindowLocation<WindowLocation["state"]>,
@@ -15,18 +16,53 @@ export function IndexDirList(props: {
 }) {
   const theme = useTheme()
   const [ref, bounds] = useMeasure({ debounce: 200 })
+  const [items, setItems] = useState([{ id: "listHeader", path: "" }].concat(props.subFolders))
+  const [moreItems, setMoreItems] = useState([])
+  // const items = [{ id: "listHeader", path: "" }].concat(props.subFolders)
+  console.log("items: ", items)
+
+  const transition = useTransition(items, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: config.stiff,
+    trail: 200,
+    onRest: () => {
+      setMoreItems(props.subFolders)
+    }
+  })
+
+  const lineTransition = useTransition(moreItems, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: config.stiff,
+    trail: 150
+  })
+
 
   return (
     <>
-      <Box ref={ref} id={"listHeader"}>
-        <IndexDirListHeader dirName={props.location.pathname} />
-      </Box>
-      {props.subFolders.map((value, index) => (
-        <Box key={index} sx={{ pl: 5 }}>
-          <IndexDirListEl arrowId={"listEl" + String(index)} dirName={props.subFolders[index].path} />
+      {transition((style, item) => item.id === "listHeader"
+        ? (
+          <animated.div style={style}>
+            <Box ref={ref} id={"listHeader"}>
+              <IndexDirListHeader dirName={props.location.pathname} />
+            </Box>
+          </animated.div>
+        )
+        : (
+          <animated.div style={style}>
+            <Box key={item.id} sx={{ pl: 5 }}>
+              <IndexDirListEl arrowId={item.id} dirName={item.path} />
+            </Box>
+          </animated.div>
+        ))}
+      {lineTransition((style, item) => (
+        <animated.div style={style}>
           <Xarrow
             start={"listHeader"}
-            end={"listEl" + String(index)}
+            end={item.id}
             lineColor={theme.palette.primary.main}
             strokeWidth={2}
             showHead={false}
@@ -37,7 +73,7 @@ export function IndexDirList(props: {
             endAnchor={"left"}
             curveness={1}
           />
-        </Box>
+        </animated.div>
       ))}
     </>
   )
