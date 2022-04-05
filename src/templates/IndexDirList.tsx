c73d4397b48a69642af57d64c99005d539e97b23
Yehuda Makarov <yehudamakarov@gmail.com>
2022-04-05 14:14:43 -0400

    useMeasure instead

--- src/templates/IndexDirList.tsx
+++ src/templates/IndexDirList.tsx
@@ -1,25 +1,20 @@
 // todo give full path and current level
 import * as React from "react"
-import { useEffect, useRef, useState } from "react"
+import { useState } from "react"
 import { WindowLocation } from "@reach/router"
 import { FoldersImmediatelyUnderQuery } from "../../gatsby-graphql"
 import { IndexDirListEl } from "./IndexDirListEl"
 import { IndexDirListHeader } from "./IndexDirListHeader"
 import Xarrow from "react-xarrows"
 import { Box, useTheme } from "@mui/material"
+import useMeasure from "react-use-measure"
 
 export function IndexDirList(props: {
   location: WindowLocation<WindowLocation["state"]>,
   subFolders: FoldersImmediatelyUnderQuery["indexes"]["nodes"]
 }) {
   const theme = useTheme()
-  const [width, setWidth] = useState(800)
-
-  const ref = useRef(null)
-
-  useEffect(() => {
-    setWidth(ref.current.offsetWidth)
-  })
+  const [ref, bounds] = useMeasure({ debounce: 200 })
 
   return (
     <>
@@ -37,7 +32,7 @@ export function IndexDirList(props: {
             showHead={false}
             startAnchor={{
               position: "bottom",
-              offset: { x: -((width / 2) - parseInt(theme.spacing(2).slice(0, 2))) }
+              offset: { x: -((bounds.width / 2) - parseInt(theme.spacing(2).slice(0, 2))) }
             }}
             endAnchor={"left"}
             curveness={1}
