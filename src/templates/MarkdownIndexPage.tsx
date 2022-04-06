dbb8ed7594165135ff23534d105e30dcff0ac4d3
Yehuda Makarov <yehudamakarov@gmail.com>
2022-04-05 23:28:27 -0400

    excellent tail for cards first

--- src/templates/MarkdownIndexPage.tsx
+++ src/templates/MarkdownIndexPage.tsx
@@ -1,14 +1,18 @@
 import * as React from "react"
 import { navigate, PageProps } from "gatsby"
 import { Layout } from "../components/layout/Layout"
-import { Box, Chip, Container, Typography } from "@mui/material"
+import { Box, Chip, Container, Grid, Typography, useTheme } from "@mui/material"
 import { MarkdownIndexContext } from "./MarkdownIndexPageTemplate"
-import { IndexDirList } from "./IndexDirList"
 import { FoldersImmediatelyUnderQuery } from "../../gatsby-graphql"
-import { IndexCardList } from "./IndexCardList"
 import * as qs from "query-string"
 import { MarkdownPageContext } from "../../config/util/createArticlePages"
 import { WindowLocation } from "@reach/router"
+import useMeasure from "react-use-measure"
+import { IndexDirListHeader } from "./IndexDirListHeader"
+import { IndexDirListEl } from "./IndexDirListEl"
+import Xarrow from "react-xarrows"
+import { ArticleIndexCard } from "./ArticleIndexCard"
+import { animated, config, useTrail } from "react-spring"
 
 function getPathname(location: WindowLocation<unknown>, value: string) {
   let lastPositionToRemove = location.search.indexOf(value) + value.length
@@ -28,11 +32,14 @@ function getPathname(location: WindowLocation<unknown>, value: string) {
 }
 
 export default function MarkdownIndexPage(props: { pageProps: PageProps<FoldersImmediatelyUnderQuery, MarkdownIndexContext> }) {
+  const theme = useTheme()
+  const [ref, bounds] = useMeasure({ debounce: 200 })
+
   let cards = props.pageProps.data.contentPages.nodes
+  console.log("initial cards", cards)
   let tags = []
 
   const params = qs.parse(props.pageProps.location.search)
-
   if (params.tag) {
     tags = typeof params.tag === "string" ? [params.tag] : params.tag
     cards = cards
@@ -44,15 +51,59 @@ export default function MarkdownIndexPage(props: { pageProps: PageProps<FoldersI
           )
         )
       )
+    console.log("filtered: ", cards)
   }
+  const filteringByTag = tags.length > 0
+  console.log("filteringByTag: ", filteringByTag)
+
+  const subFolders = props.pageProps.data.indexes.nodes
+  const listItems = [{ id: "listHeader", path: "" }].concat(subFolders)
+
+  // =============================================================================== //
+
+  console.log("length of cards: ", cards.length)
+  const cardTrail = useTrail(cards.length, {
+    from: { opacity: 0, transform: "translate(0, 50vh)" },
+    to: { opacity: 1, transform: "translate(0, 0)" },
+    config: config.stiff,
+    reset: filteringByTag
+  })
+
+  // =============================================================================== //
 
   return (
     <Layout pageProps={props.pageProps}>
       <Container>
         <Box sx={{ mt: 4 }}>
-          <IndexDirList location={props.pageProps.location} subFolders={props.pageProps.data.indexes.nodes} />
+          {listItems.map(item => item.id === "listHeader"
+            ? (
+              <Box key={"listHeader"} ref={ref} id={"listHeader"}>
+                <IndexDirListHeader dirName={props.pageProps.location.pathname} />
+              </Box>
+            )
+            : (
+              <Box key={item.id} sx={{ pl: 5 }}>
+                <IndexDirListEl arrowId={item.id} dirName={item.path} />
+              </Box>
+            ))}
+          {subFolders.map(item => (
+            <Xarrow
+              start={"listHeader"}
+              end={item.id}
+              lineColor={theme.palette.primary.main}
+              strokeWidth={2}
+              showHead={false}
+              startAnchor={{
+                position: "bottom",
+                offset: { x: -((bounds.width / 2) - parseInt(theme.spacing(2).slice(0, 2))) }
+              }}
+              endAnchor={"left"}
+              curveness={1}
+              key={item.id}
+            />
+          ))}
         </Box>
-        {tags.length > 0 && (
+        {filteringByTag && (
           <Box sx={{ mt: 4 }}>
             <Typography variant={"caption"}> Filtered by: </Typography>
             {tags.map((value, index) => {
@@ -72,7 +123,19 @@ export default function MarkdownIndexPage(props: { pageProps: PageProps<FoldersI
           </Box>
         )}
         <Box sx={{ mt: 4 }}>
-          <IndexCardList location={props.pageProps.location} cards={cards} />
+          <Grid container spacing={4}>
+            {cardTrail.map((style, i) => (
+              <Grid key={cards[i].id} item xs={12} sm={4}>
+                <animated.div style={style}>
+                  <ArticleIndexCard
+                    location={props.pageProps.location}
+                    path={cards[i].path}
+                    displayContent={cards[i].pageContext as MarkdownPageContext}
+                  />
+                </animated.div>
+              </Grid>
+            ))}
+          </Grid>
         </Box>
       </Container>
     </Layout>
